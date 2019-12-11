e4d52eec1338dd9439f740ded1966a2ae55dae0a
Katja Nordhoff <contralateral@gmx.net>
2019-12-11 10:09:49 +0100

    Add testFun function

---
+++ R/testFun.R
@@ -0,0 +1,20 @@
+#' Make a binary factor
+#'
+#' Create a factor vector of n elements, labeled A and B.
+#'
+#' @param n integer
+#'
+#' @return factor
+#' @export
+#'
+#' @examples
+#' testFun(76)
+
+testFun <- function(n){
+  base::factor(
+    base::sample(
+      0:1, n, TRUE
+    ),
+    labels = base::LETTERS[1:2]
+  )
+}
