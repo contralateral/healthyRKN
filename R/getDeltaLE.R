8cb8a74f43a4c63cd58316225da1ff370c765f0a
Katja Nordhoff <contralateral@gmx.net>
2019-12-11 12:00:16 +0100

    Added getDeltaLE function

--- R/getDeltaLE.R
+++ R/getDeltaLE.R
@@ -1,21 +1,23 @@
-#' Given a country from the gapminder dataset, return the increase in years of life expectancy between 1952 and 2007.
-#'
-#' @param x factor
-#'
-#' @return A vector of length 1
-#' @export
-#' @examples
-#' getDeltaLE(France)
 
+#' Given a country, return the increase in years of life expectancy between 1952 and 2007.
+#'
+#' @param gap_country charachter
+#'
+#' @return numeric vector of lenght 1
+#' @export
+#'
+#' @examples
 getDeltaLE <- function(gap_country){
 
-  exp2007 <- gapminder %>%
-    dplyr::filter(country == gap_country, year == 2007) %>%
-    dplyr::select(lifeExp)
+  exp2007 <- gapminder::gapminder %>%
+    dplyr::filter(.data$country == gap_country, .data$year == 2007) %>%
+    dplyr::select(.data$lifeExp)
 
-  exp1952 <- gapminder %>%
-    dplyr::filter(country == gap_country, year == 1952) %>%
-    dplyr::select(lifeExp)
+  exp1952 <- gapminder::gapminder %>%
+    dplyr::filter(.data$country == gap_country, .data$year == 1952) %>%
+    dplyr::select(.data$lifeExp)
   return(exp2007$lifeExp - exp1952$lifeExp)
 }
 
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c(".data", "%>%"))
